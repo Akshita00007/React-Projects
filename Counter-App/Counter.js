d28397fb9c938b258c2345688d6218adaa5d3284
Akshita Mukati <136486348+Akshita00007@users.noreply.github.com>
2023-09-22 15:19:34 +0530

    Create Counter.js

---
+++ Counter-App/Counter.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function Counter(){
+    const [x,setX]=useState(0);
+    return(
+        <>
+        count : {x}
+        <button onClick={()=> setX(x+1)}>Inc</button>
+        <button onClick={()=> setX(x-1)}>Dec</button>
+        </>
+    )
+}
+export default Counter;
